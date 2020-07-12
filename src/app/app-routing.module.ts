7d22eb2c2ed879f989f7f41242a8d63c935a8111
Elvis020 <elvis.amoako@stu.ucc.edu.gh>
2020-07-12 01:03:59 -0700

    Done with dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { UsersComponent } from './components/users/users.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { BlogComponent } from './components/blog/blog.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: BlogComponent},
+  {path: 'posts', component: PostsComponent},
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'users', component: UsersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { UsersComponent } from './components/users/users.component';
+import { CKEditorModule } from 'ckeditor4-angular';
+import { PostsComponent } from './components/posts/posts.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogComponent,
+    UsersComponent,
+    PostsComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
